25bc3b197aa1fe038ccf340ad410403d7e8cab46
Dale Bustad <dale@divmain.com>
2013-12-09 14:20:45 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var path = require("path"),
+    _ = require("underscore"),
+    app = (require("express"))(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+var trackedURLs = {};
+
+app.set("views", path.join(__dirname, "templates"))
+   .set("view engine", "hbs");
+
+
+app.get("*", function(req, res) {
+    var url = req.url;
+    startTracking(url);
+    res.render("pageview", {});
+});
+
+var startTracking = function(url) {
+    trackedURLs[url] = io.of(url);
+    trackedURLs[url].on("connection", function(client) {
+
+    });
+    checkURL(url);
+};
+
+var checkURL = function(url) {
+    // ...
+    _.delay(function(url){ checkURL(url); }, 5000);
+}
+
+var PORT = 3000
+server.listen(PORT);
+console.log("Started teamedit on port " + PORT);
