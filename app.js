bb7dcf2d5dcbb7c3b5626949746623aba52dfdeb
Aaron Pollack <pollackaaron@gmail.com>
2013-12-10 14:43:03 -0800

    Got primitive diffing logic working, used node  instead of _delay

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var path = require("path"),
     _ = require("underscore"),
     app = (require("express"))(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+    io = require('socket.io').listen(server),
+    request = require('request');
 
 var trackedURLs = {};
+
+prevHTML = null;
+intervalID = null;
 
 app.set("views", path.join(__dirname, "templates"))
    .set("view engine", "hbs");
@@ -21,15 +25,30 @@ var startTracking = function(url) {
     trackedURLs[url].on("connection", function(client) {
 
     });
-    var checkURLfunc = function(url) { checkURL(url); };
-    checkURL(checkURLfunc);
+    intervalID = setInterval(checkURL, 5000, url);
 };
 
-var checkURL = function(urlFunc) {
-    // ...
-    _.delay(urlFunc, 5000);
+var stopTracking = function() {
+    clearInterval(intervalID)
+};
+
+var checkURL = function(url) {
+    request(url, function (error, response, body) {
+        console.log('made it here')
+        if (!error && response.statusCode == 200 && body !== prevHTML) {
+            prevHTML = body;
+            console.log(prevHTML);
+            updateIframe(url);
+        }
+    })
+};
+
+var updateIframe = function (url) {
+    //Send update 
 }
 
 var PORT = 3000
 server.listen(PORT);
 console.log("Started f5yourself on port " + PORT);
+
+startTracking("http://www.homestarrunner.com/")
